9f8f27d09b2d351a74f049c721ae579405443dd3
IwamotoHinata <c612101277@tokushima-u.ac.jp>
2024-04-10 00:36:20 +0900

    MVRPの修正

--- Assets/Scripts/Scenes/InGame/Player/DisplayPlayerStatusManager.cs
+++ Assets/Scripts/Scenes/InGame/Player/DisplayPlayerStatusManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UniRx;
 using TMPro;
 using System;
@@ -9,16 +8,11 @@ using System;
 namespace Scenes.Ingame.Player
 {
     /// <summary>
-    /// PlayerStatusの情報をもとにゲーム内UIにプレイヤー全員の情報を表示させる為のスクリプト
+    /// PlayerStatusの情報をもとにゲーム内UIにプレイヤー全員の情報を表示させる為のスクリプト（データ管理を担う）
     /// 後ほどネットワークでの、複数人の情報の表示に対応できる形を心がける
-    /// MV(R)PにおけるViewの役割を想定
     /// </summary>
     public class DisplayPlayerStatusManager : MonoBehaviour
     {
-        [SerializeField] private Slider[] _healthSliders;
-        [SerializeField] private Slider[] _sanValueSliders;
-        [SerializeField] private TMP_Text[] _healthText;
-        [SerializeField] private TMP_Text[] _sanValueText;
 
         public int playerNum { get { return _players.Length; } }//ゲームに参加しているプレイヤーの人数
         [SerializeField] private GameObject[] _players;//プレイヤーのGameObjectを格納
@@ -36,38 +30,16 @@ namespace Scenes.Ingame.Player
             _playerStatuses = new PlayerStatus[_players.Length];
 
             //PlayerIDの昇順にソートを行う。
+            //今後IDは8桁の数字になるかも。その時は数の大きさで昇順にソートするようにせよ
             foreach (var player in _players)
             {
                 int myID = player.GetComponent<PlayerStatus>().playerID;
                 _playerStatuses[myID] = player.GetComponent<PlayerStatus>();
-
-                //配列のソート終了を通知する
-                _completeSort.OnNext(Unit.Default);
-                _completeSort.OnCompleted();
-            }
-        }
-        /// <summary>
-        /// Sliderの値を変える為の関数
-        /// </summary>
-        /// <param name="value">Slinder.Valueに代入する値</param>
-        /// <param name="ID">プレイヤーID</param>
-        /// <param name="mode">Health(体力), SanValue(SAN値)どちらを変更するのかを決定</param>
-        public void ChangeSliderValue(int value , int ID, string mode)
-        {
-            Debug.Log(ID);
-            if (mode == "Health")
-            {
-                _healthSliders[ID].value = value;
-                _healthText[ID].text = value.ToString();
             }
 
-            else if (mode == "SanValue")
-            { 
-                _sanValueSliders[ID].value = value;
-                _sanValueText[ID].text = value.ToString();
-            }
-                
-        }
+            //配列のソート終了を通知する
+            _completeSort.OnNext(Unit.Default);
+            _completeSort.OnCompleted();
+        }       
     }
 }
-
--- Assets/Scripts/Scenes/InGame/Player/PlayerGUIPresenter.cs
+++ Assets/Scripts/Scenes/InGame/Player/PlayerGUIPresenter.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UniRx;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Scenes.Ingame.Player
 {
@@ -16,11 +18,17 @@ namespace Scenes.Ingame.Player
         //View
         [SerializeField] private DisplayPlayerStatusManager _displayPlayerStatusManager;
 
+        //画面内のUIの列挙
+        [SerializeField] private Slider[] _healthSliders;
+        [SerializeField] private Slider[] _sanValueSliders;
+        [SerializeField] private TMP_Text[] _healthText;
+        [SerializeField] private TMP_Text[] _sanValueText;
 
         // Start is called before the first frame update
         void Awake()
         {
             _displayPlayerStatusManager.OnCompleteSort
+                .FirstOrDefault()
                 .Subscribe(_ => 
                 {
                     //DisplayPlayerStatusManagerにあるソート済みの配列を取得
@@ -33,17 +41,38 @@ namespace Scenes.Ingame.Player
                             .Subscribe(x =>
                             {
                                 //viewに反映
-                                _displayPlayerStatusManager.ChangeSliderValue(x, playerStatus.playerID, "Health");
+                                ChangeSliderValue(x, playerStatus.playerID, "Health");
                             }).AddTo(this);
 
                         playerStatus.OnPlayerSanValueChange
                             .Subscribe(x =>
                             {
                                 //viewに反映
-                                _displayPlayerStatusManager.ChangeSliderValue(x, playerStatus.playerID, "SanValue");
+                                ChangeSliderValue(x, playerStatus.playerID, "SanValue");
                             }).AddTo(this);
                     }
                 }).AddTo(this);
+        }
+
+        /// <summary>
+        /// Sliderの値を変える為の関数.Slider,Textに直接参照している
+        /// </summary>
+        /// <param name="value">Slinder.Valueに代入する値</param>
+        /// <param name="ID">プレイヤーID</param>
+        /// <param name="mode">Health(体力), SanValue(SAN値)どちらを変更するのかを決定</param>
+        public void ChangeSliderValue(int value, int ID, string mode)
+        {
+            if (mode == "Health")
+            {
+                _healthSliders[ID].value = value;
+                _healthText[ID].text = value.ToString();
+            }
+
+            else if (mode == "SanValue")
+            {
+                _sanValueSliders[ID].value = value;
+                _sanValueText[ID].text = value.ToString();
+            }
         }
     }
 }
