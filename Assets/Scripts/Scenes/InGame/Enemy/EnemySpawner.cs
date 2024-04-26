c19e6db6cd3809e75ce026b5edda6d687ef441ed
[tyunousause] <tyunousause@gmail.com>
2024-04-26 14:38:49 +0900

    マップの読み込みができた

--- Assets/Scripts/Scenes/InGame/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Scenes/InGame/Enemy/EnemySpawner.cs
@@ -8,45 +8,71 @@ namespace Scenes.Ingame.Enemy
 {
     public class EnemySpawner : MonoBehaviour
     {
-        [SerializeField] private GameObject testEnemy;
+        [Header("デバッグするかどうか")]
+        [SerializeField] private bool _DebugMode;
 
+        [Header("スキャンするマップに関して")]
+        [SerializeField]
+        private EnemyVisibilityMap _enemyVisibilityMap;
+        [SerializeField]
+        private byte _x, _z;
+        [SerializeField]
+        private float _range;
+        [SerializeField]
+        private Vector3 _centerPosition;
+
+        [Header("作成する敵のプレハブ一覧")]
+        [SerializeField] private GameObject _testEnemy;
+        
 
         // Start is called before the first frame update
         void Start()
         {
+            //マップをスキャン
+            _enemyVisibilityMap = new EnemyVisibilityMap();
+            _enemyVisibilityMap.GridMake(_x, _z, _range, _centerPosition);
+            _enemyVisibilityMap.MapScan();
+
             //テストとしてここでEnemy制作を依頼している
-            EnemySpawn(EnemyName.TestEnemy);
-            EnemySpawn(EnemyName.TestEnemy,new Vector3(10,10,10));
+            //EnemySpawn(EnemyName.TestEnemy);
+            EnemySpawn(EnemyName.TestEnemy, new Vector3(10, 10, 10));            
 
         }
 
-        // Update is called once per frame
-        public void EnemySpawn(EnemyName enemeyName)
+        /*
+        public void EnemySpawn(EnemyName enemeyName)//ポリモーフィズムには死んでもらいました
         {
             switch (enemeyName)
             {
 
                 case EnemyName.TestEnemy:
-                    GameObject.Instantiate(testEnemy);
+                    GameObject.Instantiate(_testEnemy);
+                    if (_DebugMode) Debug.Log("エネミーは制作されました");
                     break;
                 default:
                     Debug.LogError("このスクリプトに、すべての敵のプレハブが格納可能かか確認してください");
                     return;
             }
         }
+        */
 
         public void EnemySpawn(EnemyName enemeyName, Vector3 spownPosition)//位置を指定してスポーンさせたい場合
         {
+            GameObject createEnemy;
+            EnemySearch createEnemySearch;
             switch (enemeyName)
             {
 
                 case EnemyName.TestEnemy:
-                    GameObject.Instantiate(testEnemy, spownPosition, Quaternion.identity);
+                    createEnemy = GameObject.Instantiate(_testEnemy, spownPosition, Quaternion.identity);
+                    if (_DebugMode) Debug.Log("エネミーは制作されました");
                     break;
                 default:
-                    Debug.LogError("このスクリプトに、すべての敵のプレハブが格納可能かか確認してください");
+                    Debug.LogError("このスクリプトに、すべての敵のプレハブが格納可能かを確認してください");
                     return;
             }
+
+            createEnemySearch = createEnemy.GetComponent<EnemySearch>();
         }
 
     }
