a248cb4d6c438d0f250e5a15ab454d312e98f583
[tyunousause] <tyunousause@gmail.com>
2024-05-10 19:34:27 +0900

    ﻿エネミーをインゲームシーンで作成できるようにした。作成直後にプレイヤーに接近しないようにした

--- Assets/Scripts/Scenes/InGame/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Scenes/InGame/Enemy/EnemySpawner.cs
@@ -1,7 +1,13 @@
+using Scenes.Ingame.Manager;
+using Scenes.Ingame.Player;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UniRx;
+using Scenes.Ingame.Manager;
+using Scenes.Ingame.InGameSystem;
+
 
 
 namespace Scenes.Ingame.Enemy
@@ -11,21 +17,29 @@ namespace Scenes.Ingame.Enemy
     /// </summary>
     public class EnemySpawner : MonoBehaviour
     {
+        public static EnemySpawner Instance;
+
         [Header("デバッグするかどうか")]
         [SerializeField] private bool _debugMode;
         [SerializeField][Tooltip("InGameManager無しで機能させるかどうか")] private bool _nonInGameManagerMode;
         [SerializeField][Tooltip("デバッグ時に作成する敵")]private EnemyName _enemyName;
 
+        [Header("マップの設定")]
         [Header("スキャンするマップに関して")]
         [SerializeField]
+        [Tooltip("自動で生成されるので挿入しない事")]
         private EnemyVisibilityMap _enemyVisibilityMap;
         [SerializeField]
+        [Tooltip("各マス目の数")]
         private byte _x, _z;
         [SerializeField]
+        [Tooltip("マップのマス目の幅")]
         private float _range;
         [SerializeField]
+        [Tooltip("最も視界の長い敵の視界の距離")]
         private float _maxVisiviilityRange;
         [SerializeField]
+        [Tooltip("マップのマス目の最も左下のマス目の中心部")]
         private Vector3 _centerPosition;
 
         [Header("作成する敵のプレハブ一覧")]
@@ -35,9 +49,34 @@ namespace Scenes.Ingame.Enemy
         [SerializeField] private GameObject _MiGo;
 
 
+        [Header("生成する際の設定")]
+        [SerializeField] private Vector3 _enemySpawnPosition;
+
         // Start is called before the first frame update
         void Start()
         {
+            if (_nonInGameManagerMode) {
+                //マップをスキャン
+                _enemyVisibilityMap = new EnemyVisibilityMap();
+                _enemyVisibilityMap.debugMode = _debugMode;
+                _enemyVisibilityMap.maxVisivilityRange = _maxVisiviilityRange;
+                _enemyVisibilityMap.GridMake(_x, _z, _range, _centerPosition);
+                _enemyVisibilityMap.MapScan();
+
+                //テストとしてここでEnemy制作を依頼している
+                EnemySpawn(EnemyName.TestEnemy, new Vector3(-10, _centerPosition.y + 3, -10));
+
+            }
+
+            if (Instance == null)
+                Instance = this;
+            else
+                Destroy(this.gameObject);
+
+        }
+
+        public void InitialSpawn() {
+
             //マップをスキャン
             _enemyVisibilityMap = new EnemyVisibilityMap();
             _enemyVisibilityMap.debugMode = _debugMode;
@@ -45,8 +84,11 @@ namespace Scenes.Ingame.Enemy
             _enemyVisibilityMap.GridMake(_x, _z, _range, _centerPosition);
             _enemyVisibilityMap.MapScan();
 
-            //テストとしてここでEnemy制作を依頼している
-            EnemySpawn(_enemyName, new Vector3(-10, _centerPosition.y+3, -10));            
+            //ここでEnemy制作
+            EnemySpawn(EnemyName.TestEnemy,_enemySpawnPosition);
+            //敵の沸きが完了したことを知らせる
+            IngameManager.Instance.SetReady(ReadyEnum.EnemyReady);
+
         }
 
 
@@ -54,8 +96,8 @@ namespace Scenes.Ingame.Enemy
         public void EnemySpawn(EnemyName enemeyName, Vector3 spownPosition)//位置を指定してスポーンさせたい場合
         {
             GameObject createEnemy;
-            EnemySearch createEnemySearch;
             EnemyStatus createEnemyStatus;
+            EnemyVisibilityMap createEnemyVisiviityMap = _enemyVisibilityMap.DeepCopy();
             switch (enemeyName)
             {
 
@@ -82,7 +124,9 @@ namespace Scenes.Ingame.Enemy
             if (createEnemy.TryGetComponent<EnemyStatus>(out createEnemyStatus))
             {
                 if (_debugMode) Debug.Log("作成した敵にはEnemyStatusクラスがあります");
-                createEnemyStatus.Init(_enemyVisibilityMap.DeepCopy());
+                createEnemyVisiviityMap.DontApproachPlayer();
+                createEnemyStatus.Init(createEnemyVisiviityMap);
+
             }
 
         }
