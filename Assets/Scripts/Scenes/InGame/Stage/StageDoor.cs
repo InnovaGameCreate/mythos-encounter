9505153138160d77f4761a1695bb5cb43c4d7be7
tibigamePoppo <ryosukehashimoto1@gmail.com>
2024-05-13 23:03:14 +0900

    ステージのオブジェクトのNavMeshの最適化

---
+++ Assets/Scripts/Scenes/InGame/Stage/StageDoor.cs
@@ -0,0 +1,56 @@
+using Scenes.Ingame.Player;
+using UnityEngine;
+using DG.Tweening;
+
+namespace Scenes.Ingame.Stage
+{
+    public class StageDoor : MonoBehaviour, IInteractable
+    {
+        [SerializeField]
+        private bool _initialStateOpen = true;
+        private bool _isOpen = false;
+        private bool _isAnimation = false;
+        private Vector3 OPENVALUE = new Vector3(0, 90, 0);
+        public void Intract(PlayerStatus status)
+        {
+            if (Input.GetMouseButtonDown(1) && _isAnimation == false)
+            {
+                _isAnimation = true;
+                switch (_isOpen)
+                {
+                    case true:
+                        DoorClose();
+                        _isOpen = false;
+                        break;
+                    case false:
+                        DoorOpen();
+                        _isOpen = true;
+                        break;
+                }
+            }
+        }
+
+        void Awake()
+        {
+            switch (_initialStateOpen)
+            {
+                case true:
+                    DoorOpen();
+                    _isOpen = true;
+                    break;
+                case false:
+                    DoorClose();
+                    _isOpen = false;
+                    break;
+            }
+        }
+        private void DoorOpen()
+        {
+            transform.DORotate(OPENVALUE, 1).SetRelative(true).SetEase(Ease.InOutSine).OnComplete(() => _isAnimation = false);
+        }
+        private void DoorClose()
+        {
+            transform.DORotate(-OPENVALUE, 1).SetRelative(true).SetEase(Ease.InOutSine).OnComplete(() => _isAnimation = false);
+        }
+    }
+}
