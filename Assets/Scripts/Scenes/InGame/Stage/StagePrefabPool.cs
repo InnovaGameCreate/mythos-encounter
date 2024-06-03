1375ebece92783f430f36e663805b76c217dc18a
tibigamePoppo <ryosukehashimoto1@gmail.com>
2024-06-04 07:44:46 +0900

    コードの整形

--- Assets/Scripts/Scenes/InGame/Stage/StagePrefabPool.cs
+++ Assets/Scripts/Scenes/InGame/Stage/StagePrefabPool.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Scenes.Ingame.Stage
@@ -54,25 +52,25 @@ namespace Scenes.Ingame.Stage
         [SerializeField]
         private GameObject _2x2CorridorPrefab;
 
-        public GameObject getTilePrefab { get => tilePrefab;}
-        public GameObject getOutSideWallXPrefab { get => outSideWallXPrefab;  }
-        public GameObject getOutSideWallYPrefab { get => outSideWallYPrefab;  }
+        public GameObject getTilePrefab { get => tilePrefab; }
+        public GameObject getOutSideWallXPrefab { get => outSideWallXPrefab; }
+        public GameObject getOutSideWallYPrefab { get => outSideWallYPrefab; }
         public GameObject getInSideWallXPrefab { get => inSideWallXPrefab; }
         public GameObject getInSideWallYPrefab { get => inSideWallYPrefab; }
-        public GameObject getWallXDoorPrefab { get => wallXDoorPrefab;  }
-        public GameObject getWallYDoorPrefab { get => wallYDoorPrefab;}
+        public GameObject getWallXDoorPrefab { get => wallXDoorPrefab; }
+        public GameObject getWallYDoorPrefab { get => wallYDoorPrefab; }
         public GameObject getPlayerSpawnRoomPrefab { get => playerSpawnRoomPrefab; }
-        public GameObject get3x3RoomPrefab { get => _3x3RoomPrefab;}
-        public GameObject get4x3RoomPrefab { get => _4x3RoomPrefab;  }
+        public GameObject get3x3RoomPrefab { get => _3x3RoomPrefab; }
+        public GameObject get4x3RoomPrefab { get => _4x3RoomPrefab; }
         public GameObject get3x4RoomPrefab { get => _3x4RoomPrefab; }
         public GameObject get4x4RoomPrefab { get => _4x4RoomPrefab; }
-        public GameObject get2x2RoomStair1fPrefab { get => _2x2RoomStair1fPrefab;  }
+        public GameObject get2x2RoomStair1fPrefab { get => _2x2RoomStair1fPrefab; }
         public GameObject get2x2RoomStair2fPrefab { get => _2x2RoomStair2fPrefab; }
         public GameObject get3x3RoomStair1fPrefab { get => _3x3RoomStair1fPrefab; }
         public GameObject get3x3RoomStair2fPrefab { get => _3x3RoomStair2fPrefab; }
         public GameObject[] get2x2RoomPrefab { get => _2x2RoomPrefab; }
-        public GameObject[] get3x2RoomPrefab { get => _3x2RoomPrefab;  }
-        public GameObject[] get2x3RoomPrefab { get => _2x3RoomPrefab;}
+        public GameObject[] get3x2RoomPrefab { get => _3x2RoomPrefab; }
+        public GameObject[] get2x3RoomPrefab { get => _2x3RoomPrefab; }
         public GameObject get2x4CorridorPrefab { get => _2x4CorridorPrefab; }
         public GameObject get4x2CorridorPrefab { get => _4x2CorridorPrefab; }
         public GameObject get2x2CorridorPrefab { get => _2x2CorridorPrefab; }
