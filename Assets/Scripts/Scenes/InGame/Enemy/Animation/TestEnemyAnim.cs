f93ab5322411198218a31dd7623c6665b893d9e7
ShunsukeHiraki <86967899+Hiraki-Shunsuke@users.noreply.github.com>
2024-06-19 18:10:06 +0900

    クトゥルフの落とし子アニメーション実装

---
+++ Assets/Scripts/Scenes/InGame/Enemy/Animation/TestEnemyAnim.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+
+namespace Scenes.Ingame.Enemy.Animation
+{
+    public class TestEnemyAnim : EnemyAnim
+    {
+        private static readonly int IsPatrolling = Animator.StringToHash("isPatrolling");
+        private static readonly int IsSearching = Animator.StringToHash("isSearching");
+        private static readonly int IsChase = Animator.StringToHash("isChase");
+        private static readonly int IsAttack = Animator.StringToHash("isAttack");
+
+        protected override void UpdateAnimationState()
+        {
+            // アニメーション制御
+            switch (_enemyStatus.ReturnEnemyState)
+            {
+                case EnemyState.Patrolling:
+                    _animator.SetTrigger(IsPatrolling);
+                    break;
+                case EnemyState.Searching:
+                    _animator.SetTrigger(IsSearching);
+                    break;
+                case EnemyState.Chase:
+                    _animator.SetTrigger(IsChase);
+                    break;
+                case EnemyState.Attack:
+                    _animator.SetTrigger(IsAttack);
+                    break;
+                case EnemyState.None:
+                    break;
+                case EnemyState.FallBack:
+                    break;
+                case EnemyState.Special:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
