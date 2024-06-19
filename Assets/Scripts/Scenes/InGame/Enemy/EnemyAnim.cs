485a1140fd447259b2595b4a8610a2b39ab391f5
ShunsukeHiraki <86967899+Hiraki-Shunsuke@users.noreply.github.com>
2024-06-19 18:08:58 +0900

    基底クラスの一部修正

--- Assets/Scripts/Scenes/InGame/Enemy/EnemyAnim.cs
+++ Assets/Scripts/Scenes/InGame/Enemy/EnemyAnim.cs
@@ -1,32 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq.Expressions;
 using UnityEngine;
-using UniRx;
 
 namespace Scenes.Ingame.Enemy
 {
     /// <summary>
     /// 敵キャラクターのアニメーション管理基底クラス
     /// </summary>
+   
     public abstract class EnemyAnim : MonoBehaviour
     {
         protected Animator _animator;
         protected EnemyStatus _enemyStatus;
-        protected EnemyMove _enemyMove;
 
         protected virtual void Awake()
         {
             _animator = GetComponent<Animator>();
-            _enemyStatus = GetComponent<EnemyStatus>();
-            _enemyMove = GetComponent<EnemyMove>();
+            _enemyStatus = transform.parent.GetComponent<EnemyStatus>();
         }
 
-        protected virtual void FixedUpdate()
+        protected virtual void Update()
         {
             UpdateAnimationState();
         }
 
         protected abstract void UpdateAnimationState();
+
     }
 }
