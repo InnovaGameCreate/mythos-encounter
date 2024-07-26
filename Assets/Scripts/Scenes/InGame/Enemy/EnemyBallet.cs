a38284848e0ac21c5901b5e874952ea600c40a48
[tyunousause] <tyunousause@gmail.com>
2024-07-26 20:48:35 +0900

    ネームスペースを修正

--- Assets/Scripts/Scenes/InGame/Enemy/EnemyBallet.cs
+++ Assets/Scripts/Scenes/InGame/Enemy/EnemyBallet.cs
@@ -4,38 +4,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
 
-public class EnemyBallet : MonoBehaviour
+namespace Scenes.Ingame.Enemy
 {
-    [SerializeField] private int _damage;
-    [SerializeField] private float _speed;
-    [SerializeField] private float _lifeTime;
-    [SerializeField] private Rigidbody _rigidbody;
-    // Start is called before the first frame update
-    void Start()
+    public class EnemyBallet : MonoBehaviour
     {
-        this.transform.LookAt(GameObject.FindWithTag("Player").transform.position);
-        _rigidbody.velocity = transform.forward * _speed;
-        Debug.Log("”­ŽË");
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        _lifeTime -= Time.deltaTime;
-        
-        if (_lifeTime < 0) { Destroy(this.gameObject); }
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Player") { //–½’†‚¾I
-            PlayerStatus playerStatus = collision.gameObject.GetComponent<PlayerStatus>();
-            playerStatus.ChangeHealth(_damage, "Damage");
-            playerStatus.OnEnemyAttackedMeEvent.OnNext(Unit.Default);
-            Destroy(this.gameObject);
-        } else if (collision.gameObject.tag == "Stage") {
-            Destroy(this.gameObject);
+        [SerializeField] private int _damage;
+        [SerializeField] private float _speed;
+        [SerializeField] private float _lifeTime;
+        [SerializeField] private Rigidbody _rigidbody;
+        // Start is called before the first frame update
+        void Start()
+        {
+            this.transform.LookAt(GameObject.FindWithTag("Player").transform.position);
+            _rigidbody.velocity = transform.forward * _speed;
+            Debug.Log("”­ŽË");
         }
-        
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            _lifeTime -= Time.deltaTime;
+
+            if (_lifeTime < 0) { Destroy(this.gameObject); }
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.tag == "Player")
+            { //–½’†‚¾I
+                PlayerStatus playerStatus = collision.gameObject.GetComponent<PlayerStatus>();
+                playerStatus.ChangeHealth(_damage, "Damage");
+                playerStatus.OnEnemyAttackedMeEvent.OnNext(Unit.Default);
+                Destroy(this.gameObject);
+            }
+            else if (collision.gameObject.tag == "Stage")
+            {
+                Destroy(this.gameObject);
+            }
+
+        }
     }
 }
--- Assets/Scripts/Scenes/InGame/Enemy/EnemyHitScanBullet.cs
+++ Assets/Scripts/Scenes/InGame/Enemy/EnemyHitScanBullet.cs
@@ -4,34 +4,37 @@ using UnityEngine;
 using Scenes.Ingame.Player;
 using UniRx;
 
-public class EnemyHitScanBullet : MonoBehaviour
+namespace Scenes.Ingame.Enemy
 {
-    [SerializeField] private int _damage;
-    private GameObject _target;
-    private PlayerStatus _targetStatus;
-
-    // Start is called before the first frame update
-    void Start()
+    public class EnemyHitScanBullet : MonoBehaviour
     {
+        [SerializeField] private int _damage;
+        private GameObject _target;
+        private PlayerStatus _targetStatus;
 
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
+        }
 
-    }
+        // Update is called once per frame
+        void Update()
+        {
 
-    /// <summary>
-    /// 発射時にアクセスする項目
-    /// </summary>
-    /// <param name="target"></param>
-    public void Init(GameObject target,PlayerStatus targetStatus)
-    {
-        _target = target;
-        _targetStatus = targetStatus;
-        this.transform.LookAt(_target.transform.position);
-        _targetStatus.ChangeHealth(_damage, "Damage");
-        _targetStatus.OnEnemyAttackedMeEvent.OnNext(Unit.Default);
+        }
+
+        /// <summary>
+        /// 発射時にアクセスする項目
+        /// </summary>
+        /// <param name="target"></param>
+        public void Init(GameObject target, PlayerStatus targetStatus)
+        {
+            _target = target;
+            _targetStatus = targetStatus;
+            this.transform.LookAt(_target.transform.position);
+            _targetStatus.ChangeHealth(_damage, "Damage");
+            _targetStatus.OnEnemyAttackedMeEvent.OnNext(Unit.Default);
+        }
     }
 }
