95741ef8db9b739671b06262573f15685a5e8ee2
KaPiBaR-001 <136887614+KaPiBaR-001@users.noreply.github.com>
2024-08-09 16:25:07 +0900

    StageLightの修正

--- Assets/Scripts/Scenes/InGame/Stage/StageLight.cs
+++ Assets/Scripts/Scenes/InGame/Stage/StageLight.cs
@@ -10,6 +10,7 @@ namespace Scenes.Ingame.Stage
     {
         [SerializeField] private List<GameObject> _lightObject = new List<GameObject>();
         private float[] _lightStrength = new float[10];
+        private Light[] _light = new Light[10];
         private bool _isOn = false;
         private bool _isAnimation = false;
         private Vector3 SWICH = new Vector3(0, -40, 0);
@@ -24,6 +25,7 @@ namespace Scenes.Ingame.Stage
             for (int i = 0; i < _lightObject.Count; i++)
             {
                 _lightStrength[i] = _lightObject[i].GetComponent<Light>().intensity;
+                _light[i] = _lightObject[i].GetComponent<Light>();
             }
             
             _child = new GameObject[]{ transform.GetChild(1).gameObject, transform.GetChild(2).gameObject};
@@ -40,17 +42,19 @@ namespace Scenes.Ingame.Stage
             if (Input.GetMouseButtonDown(1))
             {
                 _lightCollider.isTrigger = true;
-                _isAnimation = true;
-                switch (_isOn)
+                if (_isAnimation != true)
                 {
-                    case true:
-                        LightOff();
-                        _isOn = false;
-                        break;
-                    case false:
-                        LightOn();
-                        _isOn = true;
-                        break;
+                    switch (_isOn)
+                    {
+                        case true:
+                            LightOff();
+                            _isOn = false;
+                            break;
+                        case false:
+                            LightOn();
+                            _isOn = true;
+                            break;
+                    }
                 }
             }
         }
@@ -63,25 +67,27 @@ namespace Scenes.Ingame.Stage
 
         private void LightOn()
         {
+            _isAnimation = true;
             for (int i = 0; i < 2; i++)
             {
                 _child[i].transform.DORotate(SWICH, 1).SetRelative(true).SetEase(Ease.InOutSine).OnComplete(AnimationComplete);
             }
             for (int i = 0; i < _lightObject.Count; i++)
             {
-                _lightObject[i].GetComponent<Light>().intensity = _lightStrength[i];
+                _light[i].intensity = _lightStrength[i];
 
             }
         }
 
         private void LightOff()
         {
+            _isAnimation = true;
             for (int i = 0;i < 2; i++){
                 _child[i].transform.DORotate(-SWICH, 1).SetRelative(true).SetEase(Ease.InOutSine).OnComplete(AnimationComplete);
             }
             for (int i = 0; i < _lightObject.Count; i++)
             {
-                _lightObject[i].GetComponent<Light>().intensity = 0;
+                _light[i].intensity = 0;
             } 
         }
 
