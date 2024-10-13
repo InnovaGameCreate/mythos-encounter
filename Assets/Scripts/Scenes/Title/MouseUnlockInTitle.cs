bcd16fba23967c6e93a39883b8ac2260be94576d
ShunsukeHiraki <86967899+Hiraki-Shunsuke@users.noreply.github.com>
2024-10-13 14:35:20 +0900

    タイトル画面でカーソル表示されるように切り替え

---
+++ Assets/Scripts/Scenes/Title/MouseUnlockInTitle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MouseUnlockInTitle : MonoBehaviour
+{
+    void Start()
+    {
+        // 現在のシーンが「タイトル」シーンか確認
+        if (SceneManager.GetActiveScene().name == "Title")
+        {
+            // マウスロックを解除
+            UnlockCursor();
+        }
+    }
+
+    void UnlockCursor()
+    {
+        // マウスロックを解除する
+        Cursor.lockState = CursorLockMode.None;
+
+        // マウスカーソルを表示する
+        Cursor.visible = true;
+    }
+}
